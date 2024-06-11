20cf769c87468391cada767bfe2d32c12144fdf1
Diego Andres Quevedo <91376899+DiegoQuevedoTIC@users.noreply.github.com>
2024-06-10 21:22:43 -0500

    Actualizar Componente Tabla

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,7 +1,14 @@
-import { Component, Input, OnInit, Pipe, input } from '@angular/core';
-import { PersonaInterface } from '../../core/interface/persona.interface';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+
 import { DatePipe } from '@angular/common';
-import { UsuarioInterface } from '../../core/interface/usuario.interface';
 
 @Component({
   selector: 'app-tabla',
@@ -10,11 +17,42 @@ import { UsuarioInterface } from '../../core/interface/usuario.interface';
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css',
 })
-export class TablaComponent implements OnInit {
-  @Input() personas: PersonaInterface[] = [];
-  @Input() titulo: string ='';
+export class TablaComponent implements OnInit, OnChanges {
+  @Input() data: any[] = [];
+  @Input() titulo: string = '';
+  @Input() columnas: string[] = [];
 
-  ngOnInit(): void {
+  @Output() onInformacion: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onEliminar: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onEditar: EventEmitter<any> = new EventEmitter<any>();
 
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'] && changes['data'].currentValue) {
+      this.data = changes['data'].currentValue;
+    }
+  }
+
+  formatearNombreDeColumnas(columna: string): string {
+    // Dividir el nombre por mayúsculas y unir con espacios
+    return columna.replace(/([a-z])([A-Z])/g, '$1 $2').toLocaleUpperCase();
+  }
+
+  isFecha(value: any): boolean {
+    return value instanceof Date;
+  }
+
+  enviarInformacion(data: any) {
+    //Emite el evento con la información de *data*
+    this.onInformacion.emit(data);
+  }
+
+  eliminar(data: any) {
+    this.onEliminar.emit(data);
+  }
+
+  editar(data: any) {
+    this.onEditar.emit(data);
   }
 }
